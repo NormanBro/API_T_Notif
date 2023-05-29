761497af7658ec6de6b52eb0fe6a6dcc4adf01ed
NormanBro <esqueletopvp@gmail.com>
2023-05-28 23:42:35 -0700

    conf

--- src/Conf/ConfMsql.js
+++ src/Conf/ConfMsql.js
@@ -1,10 +1,10 @@
 const mysql= require('mysql2');
 
 const pool=mysql.createPool({
-    host:"aws.connect.psdb.cloud",
-    user:"02hjhs1r251yqirp2rp8",
-    database:"bd_admnotif",
-    password:"pscale_pw_hISKvkENt9nQMyxwA5qyUm0rDwQrM0hFfgY7cNeiN2Y",
+    host:"",
+    user:"",
+    database:"",
+    password:"",
     ssl:{
         rejectUnauthorized:true
     }
